187f7fac090f47d2d7c81ec82bbd32fd323c0373
Henning Krause <henningkrause96@gmx.de>
2020-03-04 22:44:31 +0100

    Improve Two Fer

--- fsharp/two-fer/TwoFer.fs
+++ fsharp/two-fer/TwoFer.fs
@@ -1,5 +1,6 @@
 ﻿module TwoFer
 
 let twoFer (input: string option): string = 
-    let name = input |> Option.defaultValue "you"
-    sprintf "One for %s, one for me." name
+    input
+    |> Option.defaultValue "you"
+    |> sprintf "One for %s, one for me."
