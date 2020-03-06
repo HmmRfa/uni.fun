af09de74ed31d64a14d01a957c13a9835986ddb4
Henning Krause <henningkrause96@gmx.de>
2020-03-06 08:10:59 +0100

    Solved Isogram

---
+++ fsharp/isogram/Isogram.fs
@@ -0,0 +1,7 @@
+﻿module Isogram
+
+let isIsogram (str:string) : bool = 
+    let formattedStr = String.map (System.Char.ToUpper) (str.Replace(" ", "").Replace("-", ""))
+    Seq.distinct formattedStr
+    |> Seq.toArray
+    |> System.String = formattedStr
