81ca2dedaf3a36ca098ef29827c8a6ff735d41ba
Henning Krause <henningkrause96@gmx.de>
2020-03-09 21:23:46 +0100

    Solve Queen-Attack

--- fsharp/queen-attack/QueenAttack.fs
+++ fsharp/queen-attack/QueenAttack.fs
@@ -1,5 +1,15 @@
 ﻿module QueenAttack
 
-let create (position: int * int) = failwith "You need to implement this function."
+let create (position: int * int) = 
+    match position with
+    | (x, y) when x < 0 || y < 0 -> false
+    | (x, y) when x < 8 && y < 8 -> true
+    | _ -> false 
 
-let canAttack (queen1: int * int) (queen2: int * int) = failwith "You need to implement this function."
+let canAttack (queen1: int * int) (queen2: int * int) = 
+    if fst queen1 = fst queen2 then
+        true
+    elif snd queen1 = snd queen2 then
+        true 
+    else
+        abs (fst queen1 - fst queen2) = abs (snd queen1 - snd queen2)
