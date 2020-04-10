bffd7bf3f419713cdaf74a9b885cd16427c7d38d
Henning Krause <henningkrause96@gmx.de>
2020-04-10 11:38:00 +0200

    Improved Raindrops

--- fsharp/raindrops/Raindrops.fs
+++ fsharp/raindrops/Raindrops.fs
@@ -7,9 +7,11 @@ let numWord = [
 ]
 
 let convert (number: int) = 
-    let wordList = List.map (fun (num, word) -> if number % num = 0 then word else "") numWord
-    let result = String.concat "" wordList
-    if result = "" then
-        string number
-    else   
-        result    
+    numWord 
+    |> List.map (fun (num, word) -> if number % num = 0 then word else "")
+    |> String.concat ""
+    |> fun resultStr ->
+        match resultStr with
+        | "" -> string number
+        | _ -> resultStr
+    
