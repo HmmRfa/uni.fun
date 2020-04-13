45694d913ec96499daa480a59b85dee4d4fa0edd
Henning Krause <henningkrause96@gmx.de>
2020-04-13 20:42:07 +0200

    Fixed Accumulate

--- fsharp/accumulate/Accumulate.fs
+++ fsharp/accumulate/Accumulate.fs
@@ -1,3 +1,16 @@
 ﻿module Accumulate
 
-let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = List.map(func) input
+let reverse list = 
+    let rec _reverse list acc =
+        match list with
+        | [] -> acc
+        | x::xs -> _reverse xs (x::acc)
+    _reverse list []    
+
+let accumulate (func: 'a -> 'b) (input: 'a list): 'b list = 
+    let rec calc f list acc =
+        match list with 
+        | [] -> acc |> reverse
+        | x::xs -> calc f xs ((f x)::acc)
+    calc func input []    
+
